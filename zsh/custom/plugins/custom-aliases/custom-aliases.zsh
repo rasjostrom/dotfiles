6d506dee58f731ff18651c0a55e5a59393da765d
rasjostrom <ras.sjostrom@hotmail.com>
2015-10-15 13:41:54 +0200

    Add custom alias plugin

---
+++ zsh/custom/plugins/custom-aliases/custom-aliases.zsh
@@ -0,0 +1,21 @@
+## System
+alias zshconfig="emacs ~/.zshrc"
+alias suspend='systemctl suspend'
+alias status='systemctl status'
+
+## Utils
+#Pastebin
+alias -g pasta="| curl -F c=@- https://ptpb.pw | sed -e 's/url: //' -n -e '/https/p' | xclip -selection c && echo 'YOUR PASTA IS READY'"
+alias -g clip="| xclip -selection c"
+alias sourcz='source ~/.zshrc'
+
+#Screen brightness
+alias xbright='xbacklight -set 100'
+alias xdark='xbacklight -set 50'
+
+##Dev-tools
+#Python Web
+alias runserver='python manage.py runserver'
+alias newdb='python manage.py makemigrations && python manage.py migrate'
+
+alias runclient='python2 -m SimpleHTTPServer 8080'
