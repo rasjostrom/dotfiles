3fb80895c0e3ad3657e4ac57084b58b0f28281a7
Rasmus Sjostrom <ras.sjostrom@hotmail.com>
2017-01-16 16:17:16 +0100

    Remove old configuration files.

--- emacs/lisp/my-docs.el
+++
@@ -1,6 +0,0 @@
-(provide 'my-docs)
-
-(add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-tools-install))
-(add-hook 'pdf-tools-install-hook
-          (lambda ()
-            (global-set-key (kbd "<f5>") 'misc/revert-buffer-no-prompt)))
